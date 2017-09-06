b0efecb131e3405177e53aad065210acb72c39f9
Jan Aertgeerts <jaertgee@its.jnj.com>
2017-09-07 00:49:53 +0200

    use promises instead of callbacks

--- api/models/sequelize/Passport.js
+++ api/models/sequelize/Passport.js
@@ -24,6 +24,7 @@ module.exports = {
                 values.password = hash
               })
             }
+          }
         },
         classMethods: {
           //If you need associations, put them here
